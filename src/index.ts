e63ab480c883f6750d90b197d4cd1703a73b516d
Martin Guillon <martin.guillon@akylas.fr>
2021-07-16 09:54:54 +0200

    chore: cleanup uncessery

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,11 @@
 import * as appCommon from '@nativescript/core/application/application-common';
 import { Application, CSSUtils, View } from '@nativescript/core';
-// import { ViewCommon, _rootModalViews } from '@nativescript/core/ui/core/view/view-common';
 import * as viewCommon from '@nativescript/core/ui/core/view/view-common';
-// import { removeCssClass, removeFromRootViewCssClasses } from '@nativescript/core/css/system-classes';
-import { Device, Screen } from '@nativescript/core/platform';
-import * as frame from '@nativescript/core/ui/frame';
 
 const removeClass = CSSUtils.removeSystemCssClass;
 
-const display = Screen.mainScreen;
 const whiteSpaceRegExp = /\s+/;
-const platformClass = `ns-${global.isAndroid ? 'android' : 'ios'}`;
-const sdkVersionClass = Device.sdkVersion.replace('.', '-');
 
-let started = false;
 
 export class ClassList {
     list: Set<string>;
@@ -105,69 +97,3 @@ viewCommon.ViewCommon.prototype._setupAsRootView = function () {
     oldSetupAsRootView.call(this, ...arguments);
     Theme.setMode(Theme.currentMode, this);
 };
-
-/* Deprecated root class setters, now available in core modules */
-function updateRootClasses(orientation, root = Application.getRootView(), classes: string[] | Set<string> = []) {
-    const classList = new ClassList(root.className);
-
-    classList.remove('ns-portrait', 'ns-landscape', 'ns-unknown', ...classes).add(`ns-${orientation}`, ...classes);
-
-    root.className = classList.get();
-}
-
-function handleOrientation({ newValue: orientation }) {
-    updateRootClasses(orientation);
-
-    if (viewCommon._rootModalViews.length) {
-        const classList = new ClassList(Application.getRootView().className);
-
-        viewCommon._rootModalViews.forEach((view) =>
-            updateRootClasses(orientation, view as View, classList.add('ns-modal').list)
-        );
-    }
-}
-
-function getOrientation() {
-    return display.heightDIPs > display.widthDIPs ? 'portrait' : 'landscape';
-}
-
-const rootModalTrap = {
-    defineProperty(target, key, desc) {
-        if (desc && 'value' in desc) {
-            target[key] = desc.value;
-
-            if (desc.value instanceof frame.Frame) {
-                const classList = new ClassList(Application.getRootView().className);
-
-                updateRootClasses(getOrientation(), desc.value, classList.add('ns-modal').list);
-            }
-        }
-
-        return target;
-    },
-};
-
-Application.on(Application.displayedEvent, () => {
-    const root = Application.getRootView();
-
-    // Bail out if no root view or root classes already set (pre 6.1).
-    if (!root || root.cssClasses.has('ns-root')) {
-        // Add ns-{platform}-{sdkVersion} classes
-        if (root) {
-            root.className = new ClassList(root.className).add(`${platformClass}__${sdkVersionClass}`).get();
-        }
-
-        return;
-    }
-
-    // Get notified when a modal is created.
-    (viewCommon as any)._rootModalViews = new Proxy(viewCommon._rootModalViews, rootModalTrap);
-
-    root.className = new ClassList(root.className).add('ns-root', platformClass, `ns-${Device.deviceType.toLowerCase()}`).get();
-
-    if (!started) {
-        handleOrientation({ newValue: getOrientation() });
-        Application.on(Application.orientationChangedEvent, handleOrientation);
-        started = true;
-    }
-});
